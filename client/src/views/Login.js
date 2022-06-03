a30ad37c76b132e683bc1639bd5e06719e8cc58c
Lisa <lb.broadhead@gmail.com>
2022-06-03 12:33:34 -0500

    first commit

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import RegistrationForm from '../components/RegistrationForm';
-import WelcomeForm from '../components/WelcomeForm';
+import { Link } from 'react-router-dom';
+import styled from '../css/Login.module.css'
 
 const Login = (props) => {
     return(
-        <div>
-            <div className="col-2">
-                <h2>Login/Register</h2>
-                <RegistrationForm />
-            </div>
-
-            <div className="col-2">
-                {/* <h2>Welcome Back</h2> */}
-                {/* <WelcomeForm/> */}
-            </div>
+        <div className={styled.banner}>
+            <div className={styled.txtWrp}>
+                <h1>Pirates</h1>
+                <Link to="/pirates" className={styled.button}>Join a Crew</Link>
+            </div>    
         </div>
     )
 }
