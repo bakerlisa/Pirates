78cb3fc22e51379c0da0e9a84aa85ce7c5cee48f
Lisa <lb.broadhead@gmail.com>
2022-06-18 11:34:50 -0500

    first commit

--- client/src/views/Resources.js
+++ client/src/views/Resources.js
@@ -7,6 +7,15 @@ const Resources = () => {
             <Banner page="Resourcespirate"  title="Pirate Resources" />
 
             <div className="resources">
+                <h2>Books</h2>
+
+                <h2>Shows</h2>
+
+                <h2>Online</h2>
+
+                <h2>Documeteries</h2>
+
+                <h2>Pop Culture</h2>
 
             </div>
         </>
